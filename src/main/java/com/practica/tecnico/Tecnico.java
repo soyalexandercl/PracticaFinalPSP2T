6879d19e6810e97b6bb574f9d3296f0c20656006
Jhon Alexander <jcl0040@alu.medac.es>
2026-02-17 22:09:42 +0100

    Commit

--- src/main/java/com/practica/tecnico/Tecnico.java
+++ src/main/java/com/practica/tecnico/Tecnico.java
@@ -4,6 +4,7 @@ import com.practica.servidor.Servidor;
 import com.practica.util.Ticket;
 
 public class Tecnico extends Thread {
+
     private String nombre;
     private Servidor servidor;
 
@@ -11,24 +12,29 @@ public class Tecnico extends Thread {
         this.nombre = nombre;
         this.servidor = servidor;
     }
-    
+
     @Override
     public void run() {
-        System.out.println("Tenico iniciado");
-        
+        System.out.println("Técnico iniciado");
         try {
             while (true) {
+                // 1. Tomar el ticket (Ya viene como EN_PROCESO desde el servidor)
                 Ticket ticket = servidor.tomarTicket(this.nombre);
-                
+
+                // 2. Notificar al cliente que hemos empezado
                 servidor.notificarCliente(ticket);
-                
+
                 System.out.println("[TÉCNICO " + nombre + "] Atendiendo Ticket #" + ticket.getId());
-                Thread.sleep(10000);
-                
+
+                // 3. Simular el tiempo de trabajo
+                Thread.sleep(50000);
+
+                // 4. CORRECCIÓN: Cambiar a RESUELTO (antes tenías EN_PROCESO aquí)
                 ticket.setEstado("RESUELTO");
-                
+
+                // 5. Notificar la resolución
                 servidor.notificarCliente(ticket);
-                
+
                 System.out.println("[TÉCNICO " + nombre + "] Ticket #" + ticket.getId() + " RESUELTO.");
             }
         } catch (InterruptedException e) {
